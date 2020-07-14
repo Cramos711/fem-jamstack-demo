3beaf2793700b831918ff81170e3fea832f7972d
Chris Ramos <Cramos7114@gmail.com>
2020-07-14 12:56:18 -0400

    feat(todos): added text styles

--- sections/todos/src/components/todo.js
+++ sections/todos/src/components/todo.js
@@ -13,6 +13,11 @@ const Todo = ({ todo, reloadTodos }) => {
       })
       .then(reloadTodos)
   }
+
+  const handleDelete = () => {
+    axios.post('/api/delete-todo', {id: todo._id }).then(reloadTodos)
+  }
+
   return (
     <>
       <label 
@@ -30,7 +35,25 @@ const Todo = ({ todo, reloadTodos }) => {
       />
       <p className = {`${styles.text} ${todo.completed && styles.completed}`}>
         {todo.text}
-      </p>      
+      </p>     
+
+
+      <label 
+        htmlFor   = {`todo-delete-${todo._id}`}
+        className = {styles.label}>
+          delete
+      </label> 
+      <button
+        onClick   = {handleDelete}
+        className = {styles.delete}>
+          <span 
+            role       = 'img'
+            aria-label = "delete"
+            title      = "delete this todo"
+          >
+            ❌
+          </span>
+        </button>
     </>
   );
 };
